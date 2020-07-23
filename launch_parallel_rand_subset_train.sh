30ad68b0566e1ac064890506a0a802a7842e118c
Karima Ma <karima@tefnut.csail.mit.edu>
2020-07-23 15:02:01 -0400

    fixed bugs in parallel launch

--- launch_parallel_rand_subset_train.sh
+++ launch_parallel_rand_subset_train.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-n_procs=10
-n_gpus=4
+n_procs=9
+n_gpus=3
 proc_id=0
 
-while [ $proc_id -lt $num_procs ]; do
-	for gpu_id in $n_gpus; do
+while [ $proc_id -lt $n_procs ]; do
+  for gpu_id in $(seq 0 $n_gpus); do
 		python3 parallel_rand_subset_train.py --gpu=$gpu_id --subset_id=$proc_id \
 		--training_subset="/home/karima/cnn-data/shuf"$proc_id"train_files.txt" \
 		--validation_file=/home/karima/cnn-data/val_file.txt &
+    echo $gpu_id $proc_id
 		pids[${proc_id}]=$!
 		proc_id=$[$proc_id+1]
 		if [ $proc_id -gt $n_procs ]; then
