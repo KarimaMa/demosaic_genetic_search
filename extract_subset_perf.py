d2b4e620c0dd9070db516e3a35b9b472f8e50b08
Karima Ma <karima@bastet.csail.mit.edu>
2020-07-24 18:20:03 -0400

    fixed extracting csv from perf data

--- extract_subset_perf.py
+++ extract_subset_perf.py
@@ -1,16 +1,23 @@
 import csv
+import argparse
 
-valid_f = open('subset_full_validation_performance', 'w', newline='\n')
+parser = argparse.ArgumentParser()
+parser.add_argument("--validation_result_file", type=str)
+parser.add_argument("--training_result_file", type=str)
+parser.add_argument("--input_data_file", type=str)
+args = parser.parse_args()
+
+valid_f = open(args.validation_result_file, 'w', newline='\n')
 valid_writer = csv.writer(valid_f, delimiter=',')
 
-train_subset_f = open('subset_train_subset_performance', 'w', newline='\n')
+train_subset_f = open(args.training_result_file, 'w', newline='\n')
 train_writer = csv.writer(train_subset_f, delimiter=',')
 
-with open("RAND_DATA_SUBSET_SELECTION/training_results", "r") as f:
+with open(args.input_data_file, "r") as f:
   for i, l in enumerate(f):
     data = l.split("training losses:")[1].strip()
-    train_data = data.split(" ")[0]
-    validation_data = data.split(" ")[1]
+    train_data = data.split("validation losses:")[0].strip()
+    validation_data = data.split("validation losses:")[1].strip()
     training_losses = [float(d.strip()) for d in train_data.split(',')]
     validation_losses = [float(d.strip()) for d in validation_data.split(',')]
     
