f60fd636b380b739e51c7b26acf6893e7ea051f8
Karima Ma <karima@tefnut.csail.mit.edu>
2020-12-01 11:59:41 -0500

    visualize new seed model

--- graph_viz.py
+++ graph_viz.py
@@ -25,21 +25,15 @@ def vis_ast_helper(root, graph, node_id):
 		child_id = node_id
 		node_id = vis_ast_helper(root.child, graph, node_id)
 		graph.edges([(str(root_id), str(child_id))])
+		
 	return node_id
 
 if __name__ == "__main__":
-	from model_lib import multires_green_model
-	import meta_model
-	full_model = meta_model.MetaModel()
-	full_model.build_default_model() 
-	green = full_model.green
+	import model_lib
+	model = model_lib.MultiresGreenQuadModel(2,10)
 
-	multires = multires_green_model()
-	graph = vis_ast(multires, 'multires_model')
-	multires.compute_size(set(), count_all_inputs=True)
-	print(multires.size)
+	graph = vis_ast(model, 'seed_model')
+	model.compute_size(set(), count_all_inputs=True)
+	print(model.size)
 	graph.render()
 
-
-	graph = vis_ast(green, "basic_model")
-	graph.render()
