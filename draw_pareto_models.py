897c920286e7220e788ad83da4a83465f10bfe98
Karima Ma <karima@tefnut.csail.mit.edu>
2020-12-04 09:12:45 -0500

    adding pareto model drawing script

---
+++ draw_pareto_models.py
@@ -0,0 +1,71 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import math
+import os
+from graph_viz import *
+import argparse
+import demosaic_ast
+from cost import ModelEvaluator
+import util
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--model_dir", type=str, help="directory with model info")
+	parser.add_argument("--pareto_ids", type=str, help="file with list of pareto model ids")
+	parser.add_argument("--visfolder", type=str, help="where to save graph drawings")
+	parser.add_argument("--model_inits", type=int )
+	parser.add_argument("--num_models", type=int, help="number of pareto models")
+	args = parser.parse_args()
+
+	w = math.ceil(math.sqrt(args.num_models))
+	f, axarr = plt.subplots(w, w)
+
+	cost_evaluator = ModelEvaluator(None)
+
+	with open(args.pareto_ids, "r") as f:
+		for i,l in enumerate(f):
+			model_id = l.strip()
+			model_subdir = os.path.join(args.model_dir, f"{model_id}")
+			model = demosaic_ast.load_ast(f"{model_subdir}/model_ast")
+
+			cost = cost_evaluator.compute_cost(model)
+			
+			init_psnrs = []
+			for init in range(args.model_inits):
+				loss_file = os.path.join(model_subdir, f"v{init}_validation_log")
+				if not os.path.exists(loss_file):
+					print(f"model id {model_id} does not have validation v{init} info")
+					continue
+				else:
+					psnrs = []
+					for l in open(loss_file, "r"):
+						loss = float(l.split(' ')[-1].strip())
+						retrain_psnr = util.compute_psnr(loss)
+						psnrs.append(retrain_psnr)
+
+					if len(psnrs) == 0:
+						continue
+					best_psnr = max(psnrs)
+
+				init_psnrs.append(best_psnr)
+
+			if len(init_psnrs) == 0:
+				continue
+
+			best_psnr = max(init_psnrs)
+			print(f"model {model_id} cost {cost} best psnr {best_psnr}")
+
+			graph = vis_ast(model, f"pareto_model_{model_id}")
+			visname = f"{args.visfolder}/pareto_model_{model_id}"
+			vispng = visname + ".png"
+			graph.render(visname)
+
+			xloc = i % w
+			yloc = i // w 
+			axarr[yloc, xloc].imshow(plt.imread(vispng))
+	
+
+	plt.savefig(os.path.join(args.visfolder, "table"))
+
--- graph_viz.py
+++ graph_viz.py
@@ -1,36 +1,53 @@
-import torch
 from graphviz import Digraph
 
 
 def vis_ast(root, name):
-	graph = Digraph(name=name)
+	graph = Digraph(name=name, format='png')
 	vis_ast_helper(root, graph, 0)
 	return graph
 
-def vis_ast_helper(root, graph, node_id):
-	graph.node(str(node_id), root.name)
-	root_id = node_id
+def vis_ast_helper(root, graph, node_id, seen=None):
+	if seen is None:
+		seen = {}
 
-	node_id += 1
-	if root.num_children == 2:
-		lchild_id = node_id
-		node_id = vis_ast_helper(root.lchild, graph, node_id)
+	if id(root) in seen:
+		return node_id
+	else:
+		root_id = node_id
+		seen[id(root)] = root_id
+		graph.node(str(node_id), root.name)
+		node_id += 1
 
-		rchild_id = node_id
-		node_id = vis_ast_helper(root.rchild, graph, node_id)		
+		if root.num_children == 2:
+			if id(root.lchild) in seen:
+				lchild_id = seen[id(root.lchild)]
+			else:	
+				lchild_id = node_id	
+				node_id = vis_ast_helper(root.lchild, graph, node_id, seen)
+			if id(root.rchild) in seen:
+				rchild_id = seen[id(root.rchild)]
+			else:
+				rchild_id = node_id
+				node_id = vis_ast_helper(root.rchild, graph, node_id, seen)		
 
-		graph.edges([(str(root_id), str(lchild_id)), (str(root_id), str(rchild_id))])
+			#graph.edges([(str(root_id), str(lchild_id)), (str(root_id), str(rchild_id))])
+			graph.edges([(str(lchild_id), str(root_id)), (str(rchild_id), str(root_id))])
 
-	elif root.num_children == 1:
-		child_id = node_id
-		node_id = vis_ast_helper(root.child, graph, node_id)
-		graph.edges([(str(root_id), str(child_id))])
-		
+
+		elif root.num_children == 1:
+			if id(root.child) in seen:
+				child_id = seen[id(root.child)]
+			else:
+				child_id = node_id
+				node_id = vis_ast_helper(root.child, graph, node_id, seen)
+			#graph.edges([(str(root_id), str(child_id))])
+			graph.edges([(str(child_id), str(root_id))])
+
 	return node_id
 
 if __name__ == "__main__":
 	import model_lib
-	model = model_lib.MultiresGreenQuadModel(2,10)
+	model = model_lib.DecoupleExampleModel(2,10)
 
 	graph = vis_ast(model, 'seed_model')
 	model.compute_size(set(), count_all_inputs=True)
