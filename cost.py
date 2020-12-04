78f8107d5d5ef81dacc2f21bed9c89e6fe5559ff
Karima Ma <karima@tefnut.csail.mit.edu>
2020-12-04 09:38:12 -0500

    removed torch training from evalutor - unused

--- cost.py
+++ cost.py
@@ -2,14 +2,9 @@
 Computes per pixel cost of a program tree
 """
 import random
-import util
 import operator
 import logging
 import argparse
-import torch.nn as nn
-import torch.utils
-import torch.backends.cudnn as cudnn
-from torch.autograd import Variable
 import numpy as np 
 
 from demosaic_ast import *
@@ -183,108 +178,6 @@ class ModelEvaluator():
 	def __init__(self, training_args):
 		self.args = training_args
 		self.log_format = '%(asctime)s %(levelname)s %(message)s'
-		
-	"""
-	trains and evaluates model
-	"""
-	def performance_cost(models, model_id, model_dir):
-		train_loggers = [util.create_logger(f'{model_id}_v{i}_train_logger', logging.INFO, \
-																			self.log_format, os.path.join(model_dir, f'v{i}_train_log'))\
-										for i in range(len(models))]
-
-		# validation_logger = [util.create_logger(f'{model_id}_validation_logger', logging.INFO, \
-		# 																	self.log_format, os.path.join(model_dir, f'v{i}_validation_log')) \
-		# 										for i in range(len(models))]
-
-		model_pytorch_files = [util.get_model_pytorch_file(model_dir, model_version) \
-													for model_version in range(len(models))]
-
-		models = [model.cuda() for model in models]
-
-		criterion = nn.MSELoss()
-
-		optimizers = [torch.optim.SGD(
-				model.parameters(),
-				self.args.learning_rate,
-				momentum=self.args.momentum,
-				weight_decay=self.args.weight_decay) for m in models]
-
-		train_data = GreenDataset(self.args.training_file) 
-		validation_data = GreenDataset(self.args.validation_file)
-
-		num_train = len(train_data)
-		train_indices = list(range(num_train*args.train_portion))
-		num_validation = len(validation_data)
-		validation_indices = list(range(num_validation))
-
-		train_queue = torch.utils.data.DataLoader(
-				train_data, batch_size=self.args.batch_size,
-				sampler=torch.utils.data.sampler.SubsetRandomSampler(train_indices),
-				pin_memory=True, num_workers=0)
-
-		validation_queue = torch.utils.data.DataLoader(
-				validation_data, batch_size=self.args.batch_size,
-				sampler=torch.utils.data.sampler.SubsetRandomSampler(validation_indices),
-				pin_memory=True, num_workers=0)
-
-		for epoch in range(args.epochs):
-			# training
-			train_losses = train(train_queue, models, criterion, optimizers, train_loggers, model_pytorch_files)
-			# validation
-			valid_losses = infer(validation_queue, models, criterion)
-
-		return valid_obj
-
-	def train(train_queue, models, criterion, optimizers, train_loggers, model_pytorch_files):
-		loss_trackers = [utils.AvgrageMeter() for m in models]
-		for m in models:
-			m.train()
-
-		for step, (input, target) in enumerate(train_queue):
-			n = input.size(0)
-
-			input = Variable(input, requires_grad=False).cuda()
-			target = Variable(target, requires_grad=False).cuda()
-
-			for i, model in enumerate(models):
-				optimizers[i].zero_grad()
-				pred = model(input)
-				loss = criterion(pred, target)
-
-				loss.backward()
-				optimizers[i].step()
-
-				loss_trackers[i].update(loss.data[0], n)
-
-			if step % self.args.report_freq == 0:
-				for i in range(len(models)):
-					train_loggers[i].info('train %03d %e', step, loss_trackers[i].avg)
-
-			if step % self.args.save_freq == 0:
-				for i in range(len(models)):
-					torch.save(models[i], model_pytorch_files[i])
-
-		return [loss_tracker.avg for loss_tracker in loss_trackers]
-
-
-	def infer(valid_queue, models, criterion):
-		loss_trackers = [utils.AvgrageMeter() for m in models]
-		for m in models:
-			model.eval()
-
-		for step, (input, target) in enumerate(valid_queue):
-			input = Variable(input, volatile=True).cuda()
-			target = Variable(target, volatile=True).cuda()
-			n = input.size(0)
-
-			for i, model in enumerate(models):
-				pred = model(input)
-				loss = criterion(pred, target)
-
-				loss_trackers[i].update(loss.data[0], n)
-
-		return [loss_tracker.avg for loss_tracker in loss_trackers]
-
 
 	def compute_cost(self, root):
 		return self.compute_cost_helper(root, set()) / 4 
