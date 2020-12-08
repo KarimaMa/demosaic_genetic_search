2e75f7cbdfe8de47728341228b902c8701c063df
Karima Ma <karima@tefnut.csail.mit.edu>
2020-12-08 14:53:46 -0500

    adding psnr lookup from training results

--- inversion_study/compute_inversions.py
+++ inversion_study/compute_inversions.py
@@ -30,6 +30,7 @@ def count_inversions(psnrs):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--sampled_models_csv", type=str, help='file with sampled model info')
+parser.add_argument("--search_models_dir", type=str, help='directory with search models')
 parser.add_argument("--retrained_models_dir", type=str, help='directory with retrained models')
 parser.add_argument("--model_inits", type=int, default=3)
 parser.add_argument("--num_tiers", type=str, help='number of cost tiers')
@@ -41,6 +42,33 @@ with open(args.sampled_models_csv,  newline='\n') as csvf:
 	reader = csv.DictReader(csvf, delimiter=',')
 	for r in reader:
 		sampled_models[r['model_id']] = {'cost': r["compute_cost"], 'psnr': r["psnr"], tier: r['tier']}
+
+for model_id, model_info in sampled_models.items():
+	model_dir = os.path.join(args.search_models_dir, f"{model_id}")
+	init_psnrs = []
+	for init in range(args.model_inits):
+		loss_file = os.path.join(model_dir, f"v{init}_validation_log")
+		if not os.path.exists(loss_file):
+			continue
+		else:
+			validation_psnrs = []
+			for l in open(loss_file, "r"):
+				loss = float(l.split(' ')[-1].strip())
+				retrain_psnr = compute_psnr(loss)
+				validation_psnrs.append(retrain_psnr)
+
+			if len(validation_psnrs) == 0:
+				continue
+			best_psnr = max(validation_psnrs)
+
+		init_psnrs.append(best_psnr)
+
+	if len(init_psnrs) == 0:
+		continue
+	new_psnr = max(init_psnrs)
+	print(f"model {model_id} search db psnr: {sampled_models[model_id]["psnr"]} stored psnr {new_psnr}")
+	sampled_models[model_id]["psnr"] = new_psnr	
+
 
 for model_id, model_info in sampled_models.items():
 	model_dir = os.path.join(args.retrained_models_dir, f"{model_id}")
