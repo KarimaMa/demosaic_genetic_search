af05a1a6d1c5842edd7b2d32a858671ded12addf
Karima Ma <karima@tefnut.csail.mit.edu>
2020-12-11 14:22:06 -0500

    revised cost calculations

--- ahd/calculate_cost.py
+++ ahd/calculate_cost.py
@@ -31,17 +31,19 @@ def median_cost(n):
 	return n * MEDIAN_FILTER_COST
 
 def norm_cost(n):
-	cost = n * POW_COST + (n-1) * ADDMUL_COST + SQRT_COST
+	cost = n * ADDMUL_COST + SQRT_COST
 	return cost
 
-def hinterp_cost():
-	green_cost = 15 * ADDMUL_COST
-	red_cost = 11 * ADDMUL_COST
-	blue_cost = 11 * ADDMUL_COST
+def hinterp_cost(green_only):
+	green_cost = 5 * ADDMUL_COST
+	red_cost = 9 * ADDMUL_COST
+	blue_cost = 9 * ADDMUL_COST
+	if green_only:
+		return green_cost
 	return green_cost + red_cost + blue_cost
 
-def vinterp_cost():
-	return hinterp_cost()
+def vinterp_cost(green_only):
+	return hinterp_cost(green_only)
 
 """
 cost of converting RGB to LAB
@@ -92,38 +94,44 @@ def homogeneity_cost(delta):
 	window_w = delta * 2 + 1
 	n_ball_points = (math.pi / 4) * window_w**2 # ratio of circle area to square area
 
-	Lcost = SUB_COST + ABSVAL_COST + COMP_COST # compares difference in luminance channel
-	Ccost = 2 * SUB_COST + 2 * POW_COST + ADDMUL_COST + COMP_COST  # computes norm of difference in color (AB) space
-	Ucost = BITWISEOP_COST # bitwise AND of L and C
-	Kcost = ADDMUL_COST # adds U to running sum of ball points surrounding x within homogeneity limits 
+	# Ccost = 2 * SUB_COST + 2 * POW_COST + ADDMUL_COST + COMP_COST  # computes norm of difference in color (AB) space
+	# Ucost = BITWISEOP_COST # bitwise AND of L and C
+	# Kcost = ADDMUL_COST # adds U to running sum of ball points surrounding x within homogeneity limits 
 
-	cost += n_ball_points * (Lcost + Ccost + Ucost + Kcost)
+	#cost += n_ball_points * (Lcost + Ccost + Ucost + Kcost)
+	Lcost = (SUB_COST + ABSVAL_COST + COMP_COST) * n_ball_points # compares difference in luminance channel
+	AB_cost = n_ball_points * 4 + 13 # calculated using Andrew's formuala 
+	cost += Lcost + AB_cost
 	return cost
 
-def artifact_cost(iterations):	
+def artifact_cost(iterations, green_only):	
 	r_cost = SUB_COST + MEDIAN8_COST + ADDMUL_COST # median of R - G, then add back G
 	b_cost = SUB_COST + MEDIAN8_COST + ADDMUL_COST # median of B - G, then add back G 
 	gr_cost = SUB_COST + MEDIAN4_COST + ADDMUL_COST # median of G - R, then add back R
 	gb_cost = SUB_COST + MEDIAN4_COST + ADDMUL_COST # median of G - B, then add back B
 	g_cost = ADDMUL_COST + DIV_COST # average G adjusted by R and B
 
-	cost = iterations * (r_cost + b_cost + gr_cost + gb_cost + g_cost)
+	if green_only:
+		cost = iterations * (gr_cost + gb_cost + g_cost)
+	else:
+		cost = iterations * (r_cost + b_cost + gr_cost + gb_cost + g_cost)
 	return cost
 
-def compute_cost(delta, iterations):
-	cost = hinterp_cost()
-	print(f"h interp cost {hinterp_cost()}")
-	cost += vinterp_cost()
-	print(f"v interp cost {vinterp_cost()}")
+def compute_cost(delta, iterations, green_only, LAB):
+	cost = hinterp_cost(green_only)
+	print(f"h interp cost {hinterp_cost(green_only)}")
+	cost += vinterp_cost(green_only)
+	print(f"v interp cost {vinterp_cost(green_only)}")
 
-	cost += 2*lab_conversion_cost() # LAB conversion of h and v images
-	print(f"lab_conversion_cost {lab_conversion_cost()}")
+	if LAB:
+		cost += 2*lab_conversion_cost() # LAB conversion of h and v images
+		print(f"lab_conversion_cost {2*lab_conversion_cost()}")
 
 	cost += epsilon_cost()
 	print(f"epsilon_cost {epsilon_cost()}")
 
 	cost += 2 * homogeneity_cost(delta) # Homogeneity maps of h and v images
-	print(f"homogeneity_cost {homogeneity_cost(delta)}")
+	print(f"homogeneity_cost {homogeneity_cost(delta):.2f}")
 
 	average_hmap_cost = 9 * ADDMUL_COST # box blur of homogeneity maps
 	average_vmap_cost = average_hmap_cost
@@ -133,8 +141,8 @@ def compute_cost(delta, iterations):
 	homogeneity_comp_cost = COMP_COST + SELECT_COST # set output to vertical interp if vmap > hmap
 
 	cost += homogeneity_comp_cost
-	cost += artifact_cost(iterations)
-	print(f"artifact_cost {artifact_cost(iterations)}")
+	cost += artifact_cost(iterations, green_only)
+	print(f"artifact_cost {artifact_cost(iterations, green_only)}")
 
 	return cost
 
@@ -143,10 +151,12 @@ def compute_cost(delta, iterations):
 parser = argparse.ArgumentParser()
 parser.add_argument("--delta", type=int)
 parser.add_argument("--iters", type=int)
+parser.add_argument("--green_only", action="store_true")
+parser.add_argument("--LAB", action="store_true")
 args = parser.parse_args()
 
 
-print(f"ahd compute cost with delta={args.delta} iterations={args.iters} {compute_cost(args.delta, args.iters)}")
+print(f"ahd compute cost with delta={args.delta} iterations={args.iters} {compute_cost(args.delta, args.iters, args.green_only, args.LAB):.2f}")
 
 
 
