d20cc24f491f89ce0acb59bebc85bfd8f0148a59
Karima Ma <karima@tefnut.csail.mit.edu>
2020-12-16 17:17:00 -0500

    adding revised training for demosaicnet

--- demosaicnet_models.py
+++ demosaicnet_models.py
@@ -2,6 +2,7 @@ import torch as th
 import torch.nn as nn
 from collections import OrderedDict
 import sys 
+import numpy as np
 
 from cost import ADD_COST, MUL_COST, RELU_COST
 
@@ -33,6 +34,10 @@ class FullDemosaicknet(nn.Module):
       n_in = width
       if i == 0:
         n_in = 4
+      """ for version with residual masking """
+      if i == depth-1:
+        n_out = 2*width
+
       layers["conv{}".format(i+1)] = nn.Conv2d(n_in, n_out, 3, padding=pad)
       layers["relu{}".format(i+1)] = nn.ReLU(inplace=True)
 
@@ -81,19 +86,38 @@ class FullDemosaicknet(nn.Module):
     """
 
     # 1/4 resolution features
+    """ my version without the residuual asking """
+    # features = self.main_processor(mosaic)
+    # residual = self.residual_predictor(features)
+
+    # # Match mosaic and residual
+    # upsampled = self.upsampler(residual)
+    # #cropped = _crop_like(mosaic, upsampled)
+
+    # packed = th.cat([mosaic, upsampled], 1)  # skip connection
+    # output = self.fullres_processor(packed)
+
+    # rgb = output * self.mask + mosaic
+
+    # return rgb
+
+
     features = self.main_processor(mosaic)
-    residual = self.residual_predictor(features)
+    filters, masks = features[:, 0:self.width], features[:, self.width:2*self.width]
+    filtered = filters * masks
+    residual = self.residual_predictor(filtered)
 
     # Match mosaic and residual
     upsampled = self.upsampler(residual)
-    #cropped = _crop_like(mosaic, upsampled)
+    cropped = _crop_like(mosaic, upsampled)
 
-    packed = th.cat([mosaic, upsampled], 1)  # skip connection
+    packed = th.cat([cropped, upsampled], 1)  # skip connection
     output = self.fullres_processor(packed)
 
     rgb = output * self.mask + mosaic
-
     return rgb
+
+
 
   def compute_cost(self):
     cost = bayer_packer_cost()
@@ -240,3 +264,26 @@ def fullres_processor_cost(width, final_out_c):
   return cost 
 
 
+def _crop_like(src, tgt):
+    """Crop a source image to match the spatial dimensions of a target.
+    Args:
+        src (th.Tensor or np.ndarray): image to be cropped
+        tgt (th.Tensor or np.ndarray): reference image
+    """
+    src_sz = np.array(src.shape)
+    tgt_sz = np.array(tgt.shape)
+
+    # Assumes the spatial dimensions are the last two
+    crop = (src_sz[-2:]-tgt_sz[-2:])
+    crop_t = crop[0] // 2
+    crop_b = crop[0] - crop_t
+    crop_l = crop[1] // 2
+    crop_r = crop[1] - crop_l
+    crop //= 2
+
+    #print(f"crop_t {crop_t} crop_b {crop_b} crop_l {crop_l} crop_r {crop_r}")
+
+    if (np.array([crop_t, crop_b, crop_r, crop_l])> 0).any():
+        return src[..., crop_t:src_sz[-2]-crop_b, crop_l:src_sz[-1]-crop_r]
+    else:
+        return src
