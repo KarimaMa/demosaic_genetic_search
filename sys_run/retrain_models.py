5ec5c3320b328c3b0e6b41592766563aa9e51cb2
Karima Ma <karima@service0001.inband>
2021-01-08 12:01:22 -0500

    fixed bug

--- sys_run/retrain_models.py
+++ sys_run/retrain_models.py
@@ -123,7 +123,7 @@ class Trainer():
     green_model_ast_file = self.args.green_model_asts[green_model_id]
     green_model_weight_file = self.args.green_model_weights[green_model_id]
 
-    green_model = demosaic_ast.load_ast(green_model_ast_file)
+    green_model = load_ast(green_model_ast_file)
 
     nodes = new_model_ast.preorder()
     for n in nodes:
