c03dca2824e659c49dfe36e92f3c2a5ad2aa3a2d
Karima Ma <karima@tefnut.csail.mit.edu>
2021-01-09 02:17:29 -0500

    adding function for computing footprint of subtree with proposed upsample insert location

--- footprint.py
+++ footprint.py
@@ -30,3 +30,35 @@ def compute_footprint_helper(self, footprint):
 
 	child_footprints = [c.compute_footprint_helper(footprint) for c in children]
 	return max(child_footprints)
+
+
+"""
+Computes the footprint of a subtree assuming a proposed upsample
+insertion right above the given node 
+"""
+def compute_lowres_branch_footprint(node):
+	footprint = compute_lowres_branch_footprint_helper(node, 1/2)
+	fullres_footprint = footprint * 2
+	return fullres_footprint
+
+def compute_lowres_branch_footprint_helper(node, footprint):
+	if type(node) is Conv2D or type(node) is Conv1D:
+		footprint += (node.kwidth // 2) 
+	elif type(node) is Upsample:
+		footprint /= 2
+	elif type(node) is Downsample:
+		footprint = footprint * 2 + 1
+
+	if node.num_children == 3:
+		children = [node.child1, node.child2, node.child3]
+	elif node.num_children == 2:
+		children = [node.lchild, node.rchild]
+	elif node.num_children == 1:
+		children = [node.child]
+	else:
+		return footprint
+
+	child_footprints = [compute_lowres_branch_footprint_helper(c, footprint) for c in children]
+	return max(child_footprints)
+
+
