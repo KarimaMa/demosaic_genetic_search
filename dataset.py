57a33278e5af38999c1e9a88942f1e683dc7d71a
Karima Ma <karima@tefnut.csail.mit.edu>
2021-01-15 08:47:54 -0500

    adding RGB8Chan search task dataset

--- dataset.py
+++ dataset.py
@@ -557,6 +557,47 @@ class FullPredictionQuadDataset(data.Dataset):
     return (input, target) 
  
 
+class RGB8ChanDataset(data.Dataset):
+  def __init__(self, data_file=None, data_filenames=None, return_index=False):
+    if data_file:
+      self.list_IDs = ids_from_file(data_file) # patch filenames
+    else:
+      self.list_IDs = data_filenames
+
+    self.return_index = return_index
+
+  def __len__(self):
+    return len(self.list_IDs)
+
+  def __getitem__(self, index):
+    image_f = self.list_IDs[index]
+    img = np.array(imread(image_f)).astype(np.float32) / (2**8-1)
+    img = np.transpose(img, [2, 0, 1])
+
+    mosaic = bayer(img)
+    mosaic = np.sum(mosaic, axis=0, keepdims=True)
+
+    quad_size = list(img.shape)
+    quad_size[0] = 4
+    quad_size[1] //= 2
+    quad_size[2] //= 2
+
+    bayer_quad = np.zeros(quad_size)
+    bayer_quad[0,:,:] = mosaic[0,0::2,0::2]
+    bayer_quad[1,:,:] = mosaic[0,0::2,1::2]
+    bayer_quad[2,:,:] = mosaic[0,1::2,0::2]
+    bayer_quad[3,:,:] = mosaic[0,1::2,1::2]
+
+    input = bayer_quad
+
+    target = img
+
+    if self.return_index:
+      return (index, input, target)
+  
+    return (input, target) 
+
+
 class GreenQuadDataset(data.Dataset):
   def __init__(self, data_file=None, data_filenames=None, return_index=False):
     if data_file:
