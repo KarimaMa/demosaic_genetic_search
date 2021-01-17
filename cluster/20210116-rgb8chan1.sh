f8ecf0951a7b0c3493440777ca4676f738835a64
Michael Gharbi <gharbi@mit.edu>
2021-01-16 21:35:17 -0800

    update jobs

--- cluster/20210116-rgb8chan1.sh
+++ cluster/20210116-rgb8chan1.sh
@@ -1,22 +1,35 @@
 #!/bin/zsh
 
+
+
+# This is needed to preempt all the GPUs on a machine...
+if [ "$1" -ne "0" ]
+then
+    echo "Not the main job, looping"
+    while true
+    do
+        printf "."
+        sleep 10
+    done
+fi
+# Only run actual job on job_id == 0
 echo "Starting worker $1"
 
 ROOT=/mnt/ilcompf9d1/user/mgharbi/code/karima
-
-echo "Installing python modules"
-export PYTHONPATH=.:$PYTHONPATH
-pip install -r requirements.txt
-
-echo "Copying data to local memory drive"
-DATA=$ROOT/data
-DATA_LOCAL=/dev/shm/data
-rsync -av $DATA /dev/shm
 
 echo "Copying code to local memory drive"
 CODE=$ROOT/demosaic_genetic_search
 CODE_LOCAL=/dev/shm/demosaic_genetic_search
 rsync -av $CODE /dev/shm
+
+echo "Installing python modules"
+export PYTHONPATH=.:$PYTHONPATH
+pip install -r $CODE_LOCAL/requirements.txt
+
+echo "Copying data to local memory drive"
+DATA=$ROOT/data
+DATA_LOCAL=/dev/shm/data
+rsync -av $DATA /dev/shm
 
 echo "Running job"
 python $CODE_LOCAL/sys_run/run.py \
