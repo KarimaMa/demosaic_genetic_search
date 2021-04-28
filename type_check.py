d28e2c141e66c28a9ef525efec02996f162a2548
Karima Ma <karima@bastet.csail.mit.edu>
2021-04-27 21:41:38 -0400

    adding function for computing spatial resolution as an scalar

--- type_check.py
+++ type_check.py
@@ -123,6 +123,37 @@ def shrink_channels(node, max_c, out_c=None):
 
 
 """
+returns none if resolution is invalid
+"""
+
+def compute_resolution(tree):
+  if isinstance(tree, Input):
+    return tree.resolution
+  elif isinstance(tree, LearnedDownsample) or isinstance(tree, Pack):
+    child_resolution = compute_resolution(tree.child)
+    return child_resolution / tree.factor 
+  elif isinstance(tree, Upsample) or isinstance(tree, LearnedUpsample) or isinstance(tree, Unpack):
+    child_resolution = compute_resolution(tree.child)
+    return child_resolution * tree.factor
+  else:
+    if tree.num_children == 3:
+      res1 = compute_resolution(tree.child1)
+      res2 = compute_resolution(tree.child2)
+      res3 = compute_resolution(tree.child3)
+      if res1 != res2 or res1 != res3:
+        return None
+      return res1
+    elif tree.num_children == 2:
+      lres = compute_resolution(tree.lchild)
+      rres = compute_resolution(tree.rchild)
+      if lres != rres:
+        return None
+      return lres
+    elif tree.num_children == 1:
+      return compute_resolution(tree.child)
+      
+
+"""
 returns the spatial resolution of a subtree
 """
 def spatial_resolution(subtree):
