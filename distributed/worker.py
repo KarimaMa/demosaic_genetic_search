9ede2c6a026cee4aaaf9359fb3ee3f4ac6d9d1be
Karima Ma <karima@bastet.csail.mit.edu>
2021-05-11 19:24:07 -0400

    testing with multiple workers

--- distributed/worker.py
+++ distributed/worker.py
@@ -5,6 +5,7 @@ import zmq
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("host")
+    parser.add_argument("worker_id")
     args = parser.parse_args()
     context = zmq.Context()
 
@@ -16,8 +17,11 @@ if __name__ == "__main__":
     #  Do 10 requests, waiting each time for a response
     for request in range(10):
         print("Sending request %s …" % request)
-        socket.send(b"Hello")
+        request_str = f"Hello from worker {args.worker_id}"
+        request = request_str.encode("utf-8")
+        socket.send(request)
 
         #  Get the reply.
         message = socket.recv()
         print("Received reply %s [ %s ]" % (request, message))
+        time.sleep(5)
