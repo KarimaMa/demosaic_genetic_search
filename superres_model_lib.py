532ad811112155ee7ceeef5d00b8a91ee78e4c14
Karima Ma <karima@bastet.csail.mit.edu>
2021-05-15 03:10:03 -0400

    fixing model resolutions

--- superres_model_lib.py
+++ superres_model_lib.py
@@ -82,22 +82,23 @@ def GreenMultires(depth, width):
 
 
 
-def ChromaDemosaicknet(depth, width):
+def ChromaDemosaicknet(depth, width, no_grad, green_model, green_model_id):
   bayer = Input(4, name="Mosaic", resolution=float(1/2))
-  green_GrGb = Input(2, name="Green@GrGb", resolution=1/2)
   rb = Input(2, name="RedBlueBayer", resolution=1/2)
   
-  flat_green = Input(1, name="GreenExtractor", resolution=1, no_grad=True, node=green_model, green_model_id=green_model_id)
+  flat_green = Input(1, name="GreenExtractor", resolution=2, no_grad=True, node=green_model, green_model_id=green_model_id)
 
   green_quad = Flat2Quad(flat_green)
   green_quad.compute_input_output_channels()
-  green_quad_input = Input(4, name="GreenQuad", resolution=1/2, node=green_quad, no_grad=True)
+  green_quad_input = Input(4, name="GreenQuad", resolution=1, node=green_quad, no_grad=True)
 
   green_rb = GreenRBExtractor(flat_green)
   green_rb.compute_input_output_channels()
-  green_rb_input = Input(2, name="Green@RB", resolution=1/2, node=green_rb, no_grad=True)
+  green_rb_input = Input(2, name="Green@RB", resolution=1, node=green_rb, no_grad=True)
 
-  rb_min_g = Sub(rb, green_rb_input)
+  rb_upsampled = LearnedUpsample(rb, 2, factor=2)
+
+  rb_min_g = Sub(rb_upsampled, green_rb_input)
   rb_min_g_stack_green = Stack(rb_min_g, green_quad_input)
   rb_min_g_stack_green.compute_input_output_channels()
   chroma_input = Input(6, name="RBdiffG_GreenQuad", resolution=1/2, no_grad=True, node=rb_min_g_stack_green)
@@ -111,22 +112,21 @@ def ChromaDemosaicknet(depth, width):
 
   # unpack the learned residual twice
   # with a conv in between to ease the growing pains
-  up1_residual = LearnedUpsample(relu, width//2, factor=2)
-  conv_up1res = Conv2D(up1_residual, width//2, kwidth=3)
-  up2_residual = LearnedUpsample(conv_up1res, width//4, factor=2)
+  fullres_residual = LearnedUpsample(relu, width//2, factor=2)
+  conv_residual = Conv2D(fullres_residual, width//2, kwidth=3)
   
   # upsample and unpack the bayer quad 
   # with a conv in between to ease the growing pains
   up1_bayer = LearnedUpsample(bayer, 4, factor=2)
   conv_up1bayer = Conv2D(up1_bayer, width//2, kwidth=3)
-  up2_bayer = LearnedUpsample(conv_up1bayer, width//4, factor=2)
+  up2_bayer = LearnedUpsample(conv_up1bayer, width//2, factor=2)
 
   # now we're in the upsampled image resolution
-  stacked = Stack(up2_bayer, up2_residual) 
+  stacked = Stack(up2_bayer, conv_residual) 
 
   highres_chroma_diff = Conv2D(stacked, 2, kwidth=3)
   chroma_pred = Add(highres_chroma_diff, flat_green)
-  out = SRGBExtractor(chroma_pred)
+  out = SRGBExtractor(flat_green, chroma_pred)
   
   out.assign_parents()
   out.compute_input_output_channels()
@@ -134,22 +134,23 @@ def ChromaDemosaicknet(depth, width):
   return out
 
 
-def ChromaMultires(depth, width):
+def ChromaMultires(depth, width, no_grad, green_model, green_model_id):
   bayer = Input(4, name="Mosaic", resolution=float(1/2))
-  green_GrGb = Input(2, name="Green@GrGb", resolution=1/2)
   rb = Input(2, name="RedBlueBayer", resolution=1/2)
   
-  flat_green = Input(1, name="GreenExtractor", resolution=1, no_grad=True, node=green_model, green_model_id=green_model_id)
+  flat_green = Input(1, name="GreenExtractor", resolution=2, no_grad=True, node=green_model, green_model_id=green_model_id)
 
   green_quad = Flat2Quad(flat_green)
   green_quad.compute_input_output_channels()
-  green_quad_input = Input(4, name="GreenQuad", resolution=1/2, node=green_quad, no_grad=True)
+  green_quad_input = Input(4, name="GreenQuad", resolution=1, node=green_quad, no_grad=True)
 
   green_rb = GreenRBExtractor(flat_green)
   green_rb.compute_input_output_channels()
-  green_rb_input = Input(2, name="Green@RB", resolution=1/2, node=green_rb, no_grad=True)
+  green_rb_input = Input(2, name="Green@RB", resolution=1, node=green_rb, no_grad=True)
+  
+  rb_upsampled = LearnedUpsample(rb, 2, factor=2)
 
-  rb_min_g = Sub(rb, green_rb_input)
+  rb_min_g = Sub(rb_upsampled, green_rb_input)
   rb_min_g_stack_green = Stack(rb_min_g, green_quad_input)
   rb_min_g_stack_green.compute_input_output_channels()
   chroma_input = Input(6, name="RBdiffG_GreenQuad", resolution=1/2, no_grad=True, node=rb_min_g_stack_green)
@@ -171,26 +172,25 @@ def ChromaMultires(depth, width):
 
   weights = Softmax(wrelu)
   weighted_interps = Mul(weights, relu)
-  summed = GroupedSum(weighted_interps, width//4)
+  summed = GroupedSum(weighted_interps, width//2)
 
   # unpack the learned residual twice
   # with a conv in between to ease the growing pains
-  up1_residual = LearnedUpsample(summed, width//4, factor=2)
-  conv_up1res = Conv2D(up1_residual, width//4, kwidth=3)
-  up2_residual = LearnedUpsample(conv_up1res, width//4, factor=2)
+  fullres_residual = LearnedUpsample(summed, width//2, factor=2)
+  conv_residual = Conv2D(fullres_residual, width//2, kwidth=3)
 
   # upsample and unpack the bayer quad 
   # with a conv in between to ease the growing pains
   up1_bayer = LearnedUpsample(bayer, 4, factor=2)
-  conv_up1bayer = Conv2D(up1_bayer, width//4, kwidth=3)
-  up2_bayer = LearnedUpsample(conv_up1bayer, width//4, factor=2)
+  conv_up1bayer = Conv2D(up1_bayer, width//2, kwidth=3)
+  up2_bayer = LearnedUpsample(conv_up1bayer, width//2, factor=2)
 
   # now we're in the upsampled image resolution
-  stacked = Stack(up2_bayer, up2_residual) 
+  stacked = Stack(up2_bayer, conv_residual) 
   highres_chroma_diff = Conv2D(stacked, 2, kwidth=3)
   chroma_pred = Add(highres_chroma_diff, flat_green)
 
-  out = SRGBExtractor(chroma_pred)
+  out = SRGBExtractor(flat_green, chroma_pred)
   # predicing all the missing values
   out.assign_parents()
   out.compute_input_output_channels()
