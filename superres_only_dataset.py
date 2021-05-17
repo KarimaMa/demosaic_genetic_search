cbc06b51e35e61a7b63bafd6556dd1970d7a7d7d
Karima Ma <karima@bastet.csail.mit.edu>
2021-05-17 00:50:55 -0400

    adding sr only dataset and seed models

---
+++ superres_only_dataset.py
@@ -0,0 +1,173 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+import numpy as np
+from torch.utils import data
+from imageio import imread
+from config import IMG_H, IMG_W
+from util import ids_from_file
+from mosaic_gen import bayer
+from superres_mosaic_gen import lowres_bayer, bicubic_downsample
+import math
+
+
+class SOnlyGreenQuadDataset(data.Dataset):
+  def __init__(self, data_file=None, return_images=False, return_index=False):
+    
+    if data_file:
+        self.list_IDs = ids_from_file(data_file)
+    else: 
+        self.input_IDs = ids_from_file(input_data_file) # patch filenames
+        self.target_IDs = ids_from_file(target_data_file)
+
+    self.return_index = return_index
+
+  def __len__(self):
+    return len(self.list_IDs)
+
+  def __getitem__(self, index):
+    image_f = self.list_IDs[index]
+    img = np.array(imread(image_f)).astype(np.float32) / (2**8-1)
+    img = np.transpose(img, [2, 0, 1])
+
+    SCALE = 2
+    kw = SCALE * 2 # for antialiasing
+    # fist input pixel that we should center our convolution taps on to produce
+    # the dowsnampled image. This is computed by normalizing the innput and output images 
+    # and finding the closest pixel in the input that corresponds to the center of the first output pixel 
+    first_input_loc = math.floor(0.5 * SCALE)
+    # the padding we need to add to force pytorch to start convolving at the desired location
+    pad = kw - first_input_loc
+    image_w = img.shape[-1]
+    # size of the downsampled image produced by pytorch BEFORE propper cropping
+    downsampled_size = (image_w + 2*pad - (2*kw+1)) // SCALE + 1
+       
+    # the relationship between convolution center pixels in the input with pixels in the output is: 
+    #  1) input_loc = output_loc * SCALE + first_input_loc 
+    # The first valid center pixel in the input that we can ask the model to predict from the downsampled image 
+    # must correspond to the first output pixel whose convolution support window is within the input image bounds:
+    #  2) input_loc >= kw to produce valid output loc
+    # Combining the two equations we have: first_valid_output_loc * SCALE + first_input_loc >= kw
+    first_valid_out_loc = math.ceil( (kw - first_input_loc) / SCALE ) 
+    # Similarly, the right most center pixel in the input that we can ask the model to predict must correspond to the 
+    # rightmost output pixel whose convolution support window is within the input image
+    #  3) input_loc <= (w-1) - kw to produce valid output loc    
+    # Combining the two equatios we have: last_valid_output_loc * SCALE + first_input_loc <= w-1 - kw
+    last_valid_out_loc = math.floor(((image_w-1 - kw) - first_input_loc) / SCALE) 
+
+    # use equation 1 to compute the corresponding leftmost and rightmost input pixel convolution centers
+    first_valid_in_loc = first_valid_out_loc * SCALE + first_input_loc
+    last_valid_in_loc = last_valid_out_loc * SCALE + first_input_loc
+
+    # Expand the bounds on the input by centering a window with size equal to the downsampling factor 
+    # on the leftmost and right most valid input convolution centers
+    # For even scale factors, we center the window between the center pixel and the pixel above or to its left 
+    # because for even scale factors, the true center of the convolution lies between input pixels 
+    # so the discrete center pixel is chosen to be the pixel to the right / below the actual center location 
+    first_valid_in_loc -= (SCALE // 2)
+    last_valid_in_loc += int((SCALE - 0.5) // 2) 
+
+    # add a batch dimension to the image to use torch convolutions for downsampling
+    batched_img = torch.Tensor(img).unsqueeze(0)
+    lowres_img = bicubic_downsample(batched_img, factor=2, pad=pad)[0]
+
+    # print(f"using pad {pad} for kernel radius {kw} and scale factor {SCALE}")
+    # print(f"computed downsampled size: {downsampled_size}  downsampled_size size before cropping : {lowres_img.shape}")
+    
+    # crop out the valid region of the lowres image
+    lowres_img = lowres_img[:,first_valid_out_loc:last_valid_out_loc+1, first_valid_out_loc:last_valid_out_loc+1]
+    
+    input = lowres_img
+  
+    target = np.expand_dims(img[1,...], axis=0)
+    # crop out the valid region of the fullres image 
+    target = target[:,first_valid_in_loc:last_valid_in_loc+1, first_valid_in_loc:last_valid_in_loc+1]
+    
+    target = torch.Tensor(target)
+
+    if self.return_index:
+        return (index, input, target)  
+    else:
+        return (input, target)
+
+
+class SOnlyRGBQuadDataset(data.Dataset):
+  def __init__(self, data_file=None, return_index=False):
+    
+    if data_file:
+        self.list_IDs = ids_from_file(data_file)
+    else: 
+        self.input_IDs = ids_from_file(input_data_file) # patch filenames
+        self.target_IDs = ids_from_file(target_data_file)
+
+    self.return_index = return_index
+
+  def __len__(self):
+    return len(self.list_IDs)
+
+  def __getitem__(self, index):
+    image_f = self.list_IDs[index]
+    img = np.array(imread(image_f)).astype(np.float32) / (2**8-1)
+    img = np.transpose(img, [2, 0, 1])
+
+    SCALE = 2
+    kw = SCALE * 2 # for antialiasing
+    # fist input pixel that we should center our convolution taps on to produce
+    # the dowsnampled image. This is computed by normalizing the innput and output images 
+    # and finding the closest pixel in the input that corresponds to the center of the first output pixel 
+    first_input_loc = math.floor(0.5 * SCALE)
+    # the padding we need to add to force pytorch to start convolving at the desired location
+    pad = kw - first_input_loc
+    image_w = img.shape[-1]
+    # size of the downsampled image produced by pytorch BEFORE propper cropping
+    downsampled_size = (image_w + 2*pad - (2*kw+1)) // SCALE + 1
+       
+    # the relationship between convolution center pixels in the input with pixels in the output is: 
+    #  1) input_loc = output_loc * SCALE + first_input_loc 
+    # The first valid center pixel in the input that we can ask the model to predict from the downsampled image 
+    # must correspond to the first output pixel whose convolution support window is within the input image bounds:
+    #  2) input_loc >= kw to produce valid output loc
+    # Combining the two equations we have: first_valid_output_loc * SCALE + first_input_loc >= kw
+    first_valid_out_loc = math.ceil( (kw - first_input_loc) / SCALE ) 
+    # Similarly, the right most center pixel in the input that we can ask the model to predict must correspond to the 
+    # rightmost output pixel whose convolution support window is within the input image
+    #  3) input_loc <= (w-1) - kw to produce valid output loc    
+    # Combining the two equatios we have: last_valid_output_loc * SCALE + first_input_loc <= w-1 - kw
+    last_valid_out_loc = math.floor(((image_w-1 - kw) - first_input_loc) / SCALE) 
+
+    # use equation 1 to compute the corresponding leftmost and rightmost input pixel convolution centers
+    first_valid_in_loc = first_valid_out_loc * SCALE + first_input_loc
+    last_valid_in_loc = last_valid_out_loc * SCALE + first_input_loc
+
+    # Expand the bounds on the input by centering a window with size equal to the downsampling factor 
+    # on the leftmost and right most valid input convolution centers
+    # For even scale factors, we center the window between the center pixel and the pixel above or to its left 
+    # because for even scale factors, the true center of the convolution lies between input pixels 
+    # so the discrete center pixel is chosen to be the pixel to the right / below the actual center location 
+    first_valid_in_loc -= (SCALE // 2)
+    last_valid_in_loc += int((SCALE - 0.5) // 2) 
+
+    # add a batch dimension to the image to use torch convolutions for downsampling
+    batched_img = torch.Tensor(img).unsqueeze(0)
+    lowres_img = bicubic_downsample(batched_img, factor=2, pad=pad)[0]
+
+    # print(f"using pad {pad} for kernel radius {kw} and scale factor {SCALE}")
+    # print(f"computed downsampled size: {downsampled_size}  downsampled_size size before cropping : {lowres_img.shape}")
+    
+    # crop out the valid region of the lowres image
+    lowres_img = lowres_img[:,first_valid_out_loc:last_valid_out_loc+1, first_valid_out_loc:last_valid_out_loc+1]
+
+    target = img
+    # crop out the valid region of the fullres image 
+    target = target[:,first_valid_in_loc:last_valid_in_loc+1, first_valid_in_loc:last_valid_in_loc+1]
+
+    target = torch.Tensor(target)
+
+    input = lowres_img
+
+    if self.return_index:
+        return (index, input, target)  
+    else:
+        return (input, target)
+
---
+++ superres_only_model_lib.py
@@ -0,0 +1,140 @@
+from demosaic_ast import *
+from type_check import compute_resolution
+
+
+def GreenNet(depth, width):
+  image = Input(3, name="Image", resolution=1)
+  for i in range(depth):
+    if i == 0:
+      conv = Conv2D(image, width, kwidth=3)
+    else:
+      conv = Conv2D(relu, width, kwidth=3)
+    relu = Relu(conv)
+
+  # upsample the residual
+  up1_residual = LearnedUpsample(relu, width//2, factor=2)
+  conv_up1res = Conv2D(up1_residual, width//2, kwidth=3)
+  
+  # upsample and unpack the image
+  up1_image = LearnedUpsample(image, 3, factor=2)
+  conv_up1image = Conv2D(up1_image, width//2, kwidth=3)
+
+  # now we're in the upsampled image resolution
+  stacked = Stack(conv_up1image, conv_up1res) 
+
+  highres_green = Conv2D(stacked, 1, kwidth=3)
+  out = SGreenExtractor(highres_green)
+  
+  out.assign_parents()
+  out.compute_input_output_channels()
+  compute_resolution(out)
+  return out
+
+
+def GreenMultires(depth, width):
+  image = Input(3, name="Image", resolution=1)
+  for i in range(depth):
+    if i == 0:
+      conv = Conv2D(image, width, kwidth=3)
+    else:
+      conv = Conv2D(relu, width, kwidth=3)
+    relu = Relu(conv) 
+
+  # weight trunk
+  image = Input(3, name="Image", resolution=1)
+  for i in range(depth):
+    if i == 0:
+      wconv = Conv2D(image, width, kwidth=3)
+    else:
+      wconv = Conv2D(wrelu, width, kwidth=3)
+    wrelu = Relu(wconv)
+
+  weights = Softmax(wrelu)
+  weighted_interps = Mul(weights, relu)
+  summed = GroupedSum(weighted_interps, 1)
+
+  up1_residual = LearnedUpsample(summed, width//4, factor=2)
+  conv_up1res = Conv2D(up1_residual, width//4, kwidth=3)
+
+  # upsample and unpack the image quad 
+  # with a conv in between to ease the growing pains
+  up1_image = LearnedUpsample(image, 3, factor=2)
+  conv_up1image = Conv2D(up1_image, width//4, kwidth=3)
+
+  # now we're in the upsampled image resolution
+  stacked = Stack(conv_up1image, conv_up1res) 
+  highres_green = Conv2D(stacked, 1, kwidth=3)
+
+  out = SGreenExtractor(highres_green)
+  # predicing all the missing values
+  out.assign_parents()
+  out.compute_input_output_channels()
+  compute_resolution(out)
+  return out
+
+
+
+def ChromaNet(depth, width, no_grad, green_model, green_model_id):
+  image = Input(4, name="Image", resolution=1)
+  
+  superres_green = Input(1, name="GreenExtractor", resolution=2, no_grad=True, node=green_model, green_model_id=green_model_id)
+  upsampled_image = LearnedUpsample(image, 3, factor=2)
+
+  chroma_input = Stack(upsampled_image, superres_green)
+
+  for i in range(depth):
+    if i == 0:
+      conv = Conv2D(chroma_input, width, kwidth=3)
+    else:
+      conv = Conv2D(relu, width, kwidth=3)
+    relu = Relu(conv)
+
+  # stack the simple upsampled image and superres green with the predicted residual
+  stacked = Stack(chroma_input, relu) 
+  superres_chroma = Conv2D(stacked, 2, kwidth=3)
+  out = SRGBExtractor(flat_green, superres_chroma)
+  
+  out.assign_parents()
+  out.compute_input_output_channels()
+  compute_resolution(out)
+  return out
+
+
+def ChromaMultires(depth, width, no_grad, green_model, green_model_id):
+  image = Input(4, name="Image", resolution=1)
+  superres_green = Input(1, name="GreenExtractor", resolution=2, no_grad=True, node=green_model, green_model_id=green_model_id)
+  upsampled_image = LearnedUpsample(image, 3, factor=2)
+
+  chroma_input = Stack(upsampled_image, superres_green)
+
+  for i in range(depth):
+    if i == 0:
+      conv = Conv2D(chroma_input, width, kwidth=3)
+    else:
+      conv = Conv2D(relu, width, kwidth=3)
+    relu = Relu(conv) 
+
+  # weight trunk
+  for i in range(depth):
+    if i == 0:
+      wconv = Conv2D(chroma_input, width, kwidth=3)
+    else:
+      wconv = Conv2D(wrelu, width, kwidth=3)
+    wrelu = Relu(wconv)
+
+  weights = Softmax(wrelu)
+  weighted_interps = Mul(weights, relu)
+  summed = GroupedSum(weighted_interps, 2)
+
+  # stack the simple upsampled image and superres green with the predicted residual
+  stacked = Stack(chroma_input, summed) 
+  # do a final conv
+  superres_chroma = Conv2D(stacked, 2, kwidth=3)
+
+  out = SRGBExtractor(flat_green, superres_chroma)
+  # predicing all the missing values
+  out.assign_parents()
+  out.compute_input_output_channels()
+  compute_resolution(out)
+  return out
+
